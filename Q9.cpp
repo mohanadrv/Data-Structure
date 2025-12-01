31784aca9e8f4584970db16c325bdd28fe784a4b
Mohanad Ahmed <nodyrofy2004@hotmail.com>
2025-12-01 12:45:39 +0200

    Add files via upload

---
+++ Q9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+
+int main() {
+    string username;
+    cin >> username;
+
+    set<char> distinct_chars;
+
+    for (char c : username) {
+        distinct_chars.insert(c);
+    }
+
+    if (distinct_chars.size() % 2 == 0) {
+        cout << "CHAT WITH HER!" << endl;
+    } else {
+        cout << "IGNORE HIM!" << endl;
+    }
+
+    return 0;
+}
